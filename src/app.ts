c3bff9e43a8a4a8a07eca0a464f1a14aad403b4a
Ramesh Kunwar <ramesh@Rameshs-MacBook-Pro.local>
2025-11-25 22:46:36 +0545

    Add cookie parser

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
 import { asyncWrapper } from "./common/utils/asyncWrapper";
+import cookieParser from "cookie-parser";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get(
     "/",
