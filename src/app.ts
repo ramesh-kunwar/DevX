d84a4128bc4ed8e4395379c4f45a9fd035bfe6be
Ramesh Kunwar <ramesh@Rameshs-MacBook-Pro.local>
2025-11-25 22:36:23 +0545

    Add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
 import express from "express";
+import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
+import { asyncWrapper } from "./common/utils/asyncWrapper";
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("hello world");
-});
+app.get(
+    "/",
+    asyncWrapper(async (req, res) => {
+        res.send("hello world");
+    }),
+);
+
+// global error handler -> should be in last
+
+app.use(globalErrorHandler);
 
 export default app;
