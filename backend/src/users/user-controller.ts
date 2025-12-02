417349194a790614753e1a96d8c3a84c70e3e339
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2025-12-02 14:46:44 +0545

    Implement cookie

--- backend/src/users/user-controller.ts
+++ backend/src/users/user-controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { UserService } from "./user-service";
+import { CONFIG } from "../config";
 
 export class UserController {
     constructor(private userService: UserService) {
@@ -14,6 +15,12 @@ export class UserController {
             // Service returns safe DTO already
             const userResponse = await this.userService.register(req.body);
 
+            res.cookie("userId", userResponse.user.id, {
+                httpOnly: true,
+                secure: CONFIG.NODE_ENV === "production",
+                sameSite: "strict",
+                maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+            });
             res.status(201).json({
                 success: true,
                 message: "User registered successfully",
@@ -26,6 +33,14 @@ export class UserController {
     async login(req: Request, res: Response, next: NextFunction) {
         try {
             const userResponse = await this.userService.login(req.body);
+
+            res.cookie("userId", userResponse.user.id, {
+                httpOnly: true,
+                secure: CONFIG.NODE_ENV === "production",
+                sameSite: "strict",
+                maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+            });
+
             res.status(200).json({
                 success: true,
                 message: "User Loggedin Successfully",
